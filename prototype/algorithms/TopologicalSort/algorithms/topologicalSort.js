bd55296a6be2c3772d7d91b2f8996b93122cdefe
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2026-05-17 01:04:38 +0530

    modified:   prototype/algorithms/TopologicalSort/algorithms/topologicalSort.js 	modified:   prototype/algorithms/TopologicalSort/test.js
    	added topological sort algorithm

--- prototype/algorithms/TopologicalSort/algorithms/topologicalSort.js
+++ prototype/algorithms/TopologicalSort/algorithms/topologicalSort.js
@@ -1,8 +1,10 @@
-const { Graph } = require ('../models/graph.js');
+// TopologicalSort.js 
+const { Graph } = require ('../models/Graph.js');
 
 class TopologicalSort {
+
   static kahn(graph) {
-    const inDegree = new Map(graph.inDegree);  // local copy
+    const inDegree = new Map(graph.inDegree);  
     const queue    = [];
     const result   = [];
 
@@ -26,31 +28,6 @@ class TopologicalSort {
     }
 
     return result;
-  }
-
-  static dfs(graph) {
-    const visited  = new Set();
-    const onStack  = new Set();  // cycle detection
-    const result   = [];
-
-    const visit = (node) => {
-      if (onStack.has(node)) throw new Error('Cycle detected');
-      if (visited.has(node)) return;
-
-      onStack.add(node);
-      for (const neighbor of graph.getNeighbors(node)) {
-        visit(neighbor);
-      }
-      onStack.delete(node);
-      visited.add(node);
-      result.push(node);   // postorder: push after all descendants
-    };
-
-    for (const node of graph.getNodes()) {
-      visit(node);
-    }
-
-    return result.reverse();  // reverse postorder = topological order
   }
 }
 
--- prototype/algorithms/TopologicalSort/test.js
+++ prototype/algorithms/TopologicalSort/test.js
@@ -1,9 +1,11 @@
-const { Graph }           = require  ('./models/graph');
-const { TopologicalSort } = require  ('./algorithms/topologicalSort');
+// index.js
+
+const { Graph }           = require ('./models/Graph.js');
+const { TopologicalSort } = require ('./algorithms/TopologicalSort.js');
 
 const graph = new Graph();
 
-// uild pipeline dependency graph
+// Example: build pipeline dependency graph
 graph.addEdge('env',    'config');
 graph.addEdge('config', 'db');
 graph.addEdge('config', 'cache');
@@ -11,5 +13,5 @@ graph.addEdge('db',     'server');
 graph.addEdge('cache',  'server');
 graph.addEdge('server', 'routes');
 
-console.log('Kahn:', TopologicalSort.kahn(graph));
-console.log('DFS: ', TopologicalSort.dfs(graph));
+const order = TopologicalSort.kahn(graph);
+console.log('Topological order:', order);
